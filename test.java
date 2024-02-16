a12a8af7cf2abbdf17b3966501eb78ce2f68e6c5
p a sai vamshi <114936994+VAMSHI70@users.noreply.github.com>
2024-02-16 22:07:55 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+ public class test
+{
+public static void main(String args[])
+{
+int a;
+System.out.println("enter a number");
+Scanner sc = new Scanner(System.in);
+a=sc.nextInt();
+System.out.println("the number is"+a); 
+}
+}
